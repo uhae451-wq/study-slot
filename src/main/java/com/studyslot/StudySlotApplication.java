45f793a89f69888862038803c871b120889f73e8
chichi <uhae451@gmail.com>
2026-08-07 21:30:35 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/studyslot/StudySlotApplication.java
@@ -0,0 +1,13 @@
+package com.studyslot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySlotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySlotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyslot/StudySlotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyslot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySlotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
